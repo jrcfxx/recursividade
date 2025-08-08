c174d52ae71c6357511aacc3de748dca4c0980aa
julia <juliarochafiorini@gmail.com>
2025-08-07 21:47:33 -0300

    Creation of Java Project

---
+++ src/main/java/com/mycompany/recursividade/Recursividade.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.recursividade;
+
+/**
+ *
+ * @author 893965
+ */
+public class Recursividade {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
